14afb4e7132211622bd744331324ddbf6297ba1f
akif hakimi <akifhakimiii@gmail.com>
2020-06-28 15:26:54 +0800

    //sama

--- busticket/dist/js/app.js
+++ busticket/dist/js/app.js
@@ -2,8 +2,51 @@ $(function(){
 
     function parseHash(newHash, oldHash){
         crossroads.parse(newHash);
-    }
 
+    }
+    
+    var routeregister = crossroads.addRoute('/register', function(){
+
+      if (sessionStorage.token) {
+        window.location.href = "#home";			
+        return;
+      }
+  
+      $.get('js/templates/register.handlebars').then(function(src) {
+        var createRegisterTemplate = Handlebars.compile(src);
+
+          $("#divcontent").empty();
+          $("#divcontent").html(createRegisterTemplate).hide().fadeIn(100);
+      });
+  
+    });
+    //add by akif 28/6/2020
+    var routelogin = crossroads.addRoute('/login', function(){
+
+      if (sessionStorage.token) {
+        window.location.href = "#home";			
+        return;
+      }
+  
+      $.get('js/templates/login.handlebars').then(function(src) {
+        var createLoginTemplate = Handlebars.compile(src);
+
+          $("#divcontent").empty();
+          $("#divcontent").html(createLoginTemplate).hide().fadeIn(100);
+
+      });
+  
+    });
+    //add by akif 28/6/2020
+    var routelogout = crossroads.addRoute('/logout', function(){
+
+      $("#loginname").html("noname");
+      sessionStorage.removeItem("token");
+      window.location.href = "#login";
+      return;
+  
+    });
+  
     var routeticket = crossroads.addRoute('/ticket', function(){
 
         $.get('js/templates/ticket.handlebars').then(function(src) {
